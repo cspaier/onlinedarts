e4b5831623ebfd7d103bdc975c96a7f6bbb1ec9a
cspaier <clement.spaier@gmail.com>
2020-04-03 09:41:16 +0200

    refactor to add a game constructor in separate file

---
+++ game.js
@@ -0,0 +1,157 @@
+function Game(){
+    // game state:
+    // 0: setting players
+    // 1: waiting for players approvals
+    // 2: playing
+    // 3: game finished
+    this.state = 0;
+    // player = { name: name, scores: scores };
+    // where scores is a list of 8 int: 20, 19, 18, 17, 16, 15, 25, S
+    this.players = [];
+    // dart ={numeroTape: numeroTape, combo: combo}
+    // volee = [{playerName: name, darts: [darts], previousScores: scores]
+    this.volees = [];
+    this.activePlayer = null;
+    this.scoresList = [ 20, 19, 18, 17, 16, 15, 25, 'S'];
+    this.alert = '<span class="oi oi-heart float-left" title="Bienvenu" aria-hidden="true"></span> Bienvenue!';
+
+    this.createPlayer = function(name){
+      // create a new player with the given name and add it to players list.
+      // change the alert
+      // A player have 2 fields: name and scores
+      // if name is already taken, we just return false
+      // test if we already have a player with this name
+      if(this.players.some(e => e.name == name)) {
+        return false;
+      }
+      // scores will be a list of 8 int: 20, 19, 18, 17, 16, 15, 25, S
+      var scores = [];
+      for (var i = 0; i <= 7; i++) {
+        scores.push(0);
+      };
+      var player = { name: name, scores: scores };
+      this.players.push(player)
+      this.alert = '<span class="oi oi-plus float-left" title="nouveau joueur" aria-hidden="true"></span> Nouveau joueur: <b>' + player.name + '</b>'
+      return true;
+    };
+
+    this.removePlayer = function(){
+      var player = this.players.pop()
+      this.alert = '<span class="oi oi-trash float-left" title="trash" aria-hidden="true"></span> Le joueur <b>' + player.name + '</b> a été supprimé.'
+    };
+
+    this.scoreVolee = function(darts){
+      this.volees.push({
+        playerName: this.activePlayer.name,
+        darts:darts,
+        previousScores: JSON.parse(JSON.stringify(this.activePlayer.scores))
+      });
+      darts.forEach((dart) => {
+        // on score chaque dart
+        // dart ={numeroTape: numeroTape, combo: combo}
+        n_initial = this.activePlayer.scores[dart.numeroTape]
+        n = n_initial + dart.combo
+        if (n > 3){
+            // on test si quelqu'un d'autre à fermé le numeroTape
+            // la boulle à une particularité: elle ne se ferme pas à plus de deux joueurs
+            if (dart.numeroTape == 6 && this.players.length > 2){
+              // c'est une boulle et on a plus de 2 joueurs
+              this.activePlayer.scores[7] += (n-3) * this.scoresList[dart.numeroTape]
+            }
+            else if (!(this.players.some(p => (p.scores[dart.numeroTape] == 3) && (p !== this.activePlayer)))){
+                this.activePlayer.scores[7] += (n-3) * this.scoresList[dart.numeroTape]
+            }
+            n = 3
+        }
+        this.activePlayer.scores[dart.numeroTape] = n
+      });
+      //test if this is over
+      if ((this.activePlayer.scores.slice(0, 7).every(s => s == 3)) && (this.players.every(p => p.scores[7] <= this.activePlayer.scores[7]))){
+        this.state = 3
+        this.alert = '<span class="oi oi-badge float-left" aria-hidden="true"></span>Le gagnant est <b>' + this.activePlayer.name + '</b>!!!'
+      } else {
+        if (darts.length == 0) {
+          alert = '<span class="oi oi-trash float-left" title="bell" aria-hidden="true"></span>'
+        } else if (darts.length == 3) {
+          alert = '<span class="oi oi-star float-left" title="pin" aria-hidden="true"></span>'
+        } else {
+          alert = '<span class="oi oi-pin float-left" title="pin" aria-hidden="true"></span>'
+        }
+        alert +=  'Le joueur <b>' + this.activePlayer.name + '</b> a fait: '
+        alert += this.dartsToString(darts)
+        this.alert = alert
+      }
+    };
+
+    this.cancelVolee = function(){
+      alert = '<span class="oi oi-action-undo float-left" title="undo" aria-hidden="true"></span>'
+      if (this.volees.length == 0) {
+        this.state = 0;
+        this.alert = alert + ' Un problème ?'
+        return true;
+      }
+      // si on est en stade 3, il faut retourner au stade 2
+      this.state = 2
+      // attention on pop la derniere volée
+      var volee = this.volees.pop()
+      alert += 'Annulation de la volée de <b>' + volee.playerName + '</b>: ' + this.dartsToString(volee.darts)
+      this.alert = alert
+      // set new activePlayer
+      player = this.players.find(p => p.name == volee.playerName)
+      this.activePlayer = player
+      this.activePlayer.scores = volee.previousScores
+    };
+
+    this.getNextPlayer = function(){
+      index = this.players.indexOf(this.activePlayer);
+      if(index >= 0 && index < this.players.length - 1){
+       nextPlayer = this.players[index + 1]
+      }
+      else{
+       nextPlayer = this.players[0]
+      }
+      return nextPlayer
+    }
+
+    this.getPreviousPlayer = function(){
+      index = this.players.indexOf(activePlayer);
+      if(index > 0){
+       previousPlayer = this.players[index - 1]
+      }
+      else{
+       previousPlayer = this.players[this.players.length - 1]
+      }
+      return previousPlayer
+    }
+
+    this.startGame = function(){
+      if (this.players.length > 0 ) {
+        this.activePlayer = this.players[0];
+        this.state = 2;
+        this.alert = '<span class="oi oi-bell float-left" title="bell" aria-hidden="true"></span> La partie a commencée.'
+        return true;
+      } else{
+        return false;
+      }
+    };
+
+    this.dartsToString = function(darts){
+        // affichage d'une voléé. Retourne un string (html)
+        html=""
+        if (darts.length == 0){
+           html += '<span class="badge badge-warning">Fromage</span>'
+        } else {
+          darts.forEach((dart, i) => {
+            if (i > 0){
+              html += ', '
+            }
+            html += '<span class="badge badge-' + ['', 'secondary', 'primary', 'danger'][dart.combo] + '">'
+            html +=  dart.combo + ' × ' + this.scoresList[dart.numeroTape] + '</span>'
+          });
+        }
+        return html
+    }
+
+
+};
+module.exports = Game
--- index.js
+++ index.js
@@ -6,13 +6,7 @@ app.use(express.static('static/'));
 var ejs = require('ejs');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-
-// game state:
-// 0: setting players
-// 1: waiting for players approvals
-// 2: playing
-// 3: game finished
+var Game = require('./game.js')
 
 // get table template as string:
 fs = require('fs')
@@ -20,88 +14,8 @@ templates = {
   tableState0: fs.readFileSync(__dirname + '/views/partials/table-state-0.ejs', 'utf-8'),
   tableState2: fs.readFileSync(__dirname + '/views/partials/table-state-2.ejs', 'utf-8'),
 }
-// dart ={numeroTape: numeroTape, combo: combo}
-// volee = [{player: name, volee: [darts]]
-var volees = []
 
-var game = {
-  // game state:
-  // 0: setting players
-  // 1: waiting for players approvals
-  // 2: playing
-  // 3: game finished
-  state: 0,
-  // player = { name: name, scores: scores };
-  // where scores is a list of 8 int: 20, 19, 18, 17, 16, 15, 25, S
-  players: [],
-  // dart ={numeroTape: numeroTape, combo: combo}
-  // volee = [{player: name, volee: [darts]]
-  volees: [],
-  activePlayer: null,
-  scoresList: [ 20, 19, 18, 17, 16, 15, 25, 'S'],
-  alert:'<span class="oi oi-heart float-left" title="Bienvenu" aria-hidden="true"></span> Bienvenue!'
-}
-
-var createPlayer = function(name){
-  // test if we already have a player with this name
-  if(game.players.some(e => e.name == name)) {
-    return false;
-  }
-  // scores will be a list of 8 int: 20, 19, 18, 17, 16, 15, 25, S
-  var scores = [];
-  for (var i = 0; i <= 7; i++) {
-    scores.push(0);
-  };
-  var player = { name: name, scores: scores };
-  return player
-};
-
-var scoreVolee = function(volee){
-  volee.forEach((dart) => {
-    // on score chaque dart
-    // dart ={numeroTape: numeroTape, combo: combo}
-    n_initial = game.activePlayer.scores[dart.numeroTape]
-    n = n_initial + dart.combo
-    if (n > 3){
-        // on test si quelqu'un d'autre à fermé le numeroTape
-        // la boulle à une particularité: elle ne se ferme pas à plus de deux joueurs
-        if (dart.numeroTape == 6 && game.players.length > 2){
-          // c'est une boulle et on a plus de 2 joueurs
-          game.activePlayer.scores[7] += (n-3) * game.scoresList[dart.numeroTape]
-        }
-        else if (!(game.players.some(p => (p.scores[dart.numeroTape] == 3) && (p !== game.activePlayer)))){
-            game.activePlayer.scores[7] += (n-3) * game.scoresList[dart.numeroTape]
-        }
-        n = 3
-    }
-    game.activePlayer.scores[dart.numeroTape] = n
-  });
-  if ((game.activePlayer.scores.slice(0, 7).every(s => s == 3)) && (game.players.every(p => p.scores[7] <= game.activePlayer.scores[7]))){
-    game.state = 3
-  }
-}
-
-var getNextPlayer = function(activePlayer){
-  index = game.players.indexOf(activePlayer);
-  if(index >= 0 && index < game.players.length - 1){
-   nextPlayer = game.players[index + 1]
-  }
-  else{
-   nextPlayer = game.players[0]
-  }
-  return nextPlayer
-}
-
-var getPreviousPlayer = function(activePlayer){
-  index = game.players.indexOf(activePlayer);
-  if(index > 0){
-   previousPlayer = game.players[index - 1]
-  }
-  else{
-   previousPlayer = game.players[game.players.length - 1]
-  }
-  return previousPlayer
-}
+var game = new Game();
 
 // La vue
 app.get('/', function(req, res){
@@ -111,76 +25,38 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   io.emit('update-game', game);
-  // all io.emit will return game object. This way we are clear game state is always same for all clients
+  // all io.emit will pass game object. This way we are clear game state is always same for all clients
   socket.on('new-player', function(playerName){
-    player = createPlayer(playerName);
-    // createPlayer will return False if playerName is already taken
-    if (player){
-      game.players.push(player)
-    }
-    game.alert = '<span class="oi oi-plus float-left" title="nouveau joueur" aria-hidden="true"></span> Nouveau joueur: <b>' + player.name + '</b>'
+    game.createPlayer(playerName);
     io.emit('change-players', game);
   });
 
   socket.on('remove-player', function(playerName){
-    var player = game.players.pop()
-    game.alert = '<span class="oi oi-trash float-left" title="trash" aria-hidden="true"></span> Le joueur <b>' + player.name + '</b> a été supprimé.'
+    game.removePlayer();
     io.emit('change-players', game);
   });
 
   socket.on('start-game', function(){
-    game.activePlayer = game.players[0];
-    game.state = 2;
-    game.alert = '<span class="oi oi-bell float-left" title="bell" aria-hidden="true"></span> La partie a commencée.'
+    game.startGame()
     io.emit('change-game-state', game);
   });
 
-  socket.on('valide-volee', function(volee){
+  socket.on('valide-volee', function(darts){
     if (game.state !== 2){
       return false
     }
-    game.volees.push({playerName: game.activePlayer.name, volee:volee, previousScores: JSON.parse(JSON.stringify(game.activePlayer.scores))});
-    scoreVolee(volee)
+    game.scoreVolee(darts)
     if (game.state == 3){
       // game is finished!
-      game.alert = '<span class="oi oi-badge float-left" aria-hidden="true"></span>Le gagnant est <b>' + game.activePlayer.name + '</b>!!!'
       io.emit('change-game-state', game);
     } else {
-      //create alert. ejs is a bit of a pain at the end :(
-      template_file = fs.readFileSync(__dirname + '/views/partials/volee.ejs', 'utf-8')
-      if (volee.length == 0) {
-        alert = '<span class="oi oi-trash float-left" title="bell" aria-hidden="true"></span>'
-      } else if (volee.length == 3) {
-        alert = '<span class="oi oi-star float-left" title="pin" aria-hidden="true"></span>'
-      } else {
-        alert = '<span class="oi oi-pin float-left" title="pin" aria-hidden="true"></span>'
-      }
-      alert +=  'Le joueur <b>' + game.activePlayer.name + '</b> a fait: '
-      alert += ejs.render(template_file, {volee: volee, scoresList: game.scoresList});
-      game.alert = alert
-      // change activePlayer
-      game.activePlayer = getNextPlayer(game.activePlayer)
+      game.activePlayer = game.getNextPlayer()
       io.emit('update-game', game);
     }
   });
 
   socket.on('cancel-volee', function(){
-    if (game.volees.length == 0) {
-      return false
-    }
-    game.state = 2
-    volee = game.volees.pop()
-    // set alert
-    alert = '<span class="oi oi-action-undo float-left" title="trash" aria-hidden="true"></span>'
-    alert += 'Annulation de la volée de <b>' + volee.playerName + '</b>: '
-    template_file = fs.readFileSync(__dirname + '/views/partials/volee.ejs', 'utf-8'),
-    alert += ejs.render(template_file, {volee: volee.volee, scoresList: game.scoresList});
-    game.alert = alert
-    // set new activePlayer
-    player = game.players.find(p => p.name == volee.playerName)
-    game.activePlayer = player
-    // attention on pop la derniere volée
-    game.activePlayer.scores = volee.previousScores
+    game.cancelVolee()
     io.emit('update-game', game)
   });
 });
