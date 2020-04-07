da85b4067cf8c47143a4ba0977a5db38e9f5dcd3
cspaier <clement.spaier@gmail.com>
2020-04-07 21:01:00 +0200

    move inline js into static files

---
+++ static/js/home.js
@@ -0,0 +1,31 @@
+$(function () {
+  var updateRooms = function(rooms){
+    var template = templates.cardRoom
+    var html = ejs.render(template, { rooms: rooms });
+    $('#rooms').html(html)
+  };
+
+  var joinRoom = function(roomId){
+    io.emit('joinRoom',roomId)
+  }
+  $('#jitsi-connect-button').click(function(){
+    $('#jitsi-connect-button').addClass('d-none');
+    var jitsiContainer = document.getElementById("jitsi-container");
+    jitsiConnect(jitsiContainer, 'onlinedarts', '');
+  })
+  var socket = io();
+
+  socket.emit('join-home')
+
+  $('#create-room').submit(function(e){
+    e.preventDefault(); // prevents page reloading
+    var roomName = $('#new-room-name').val();
+    var password = $('#new-room-password').val();
+    socket.emit('create-room', {roomName: roomName, password:password});
+    return false;
+  });
+
+  socket.on('update-rooms', function(rooms){
+    updateRooms(rooms);
+  });
+});
